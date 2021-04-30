9eb71b4ddb2ff65cc66e04f9fd34f0e3c44733ed
B1gLev <kozma.lev2004@gmail.com>
2021-04-30 11:43:12 +0200

    regex and new events

--- src/main/java/me/biglev/velocityauth/listeners/PlayerLogin.java
+++ src/main/java/me/biglev/velocityauth/listeners/PlayerLogin.java
@@ -5,30 +5,65 @@ import com.velocitypowered.api.event.Subscribe;
 import com.velocitypowered.api.event.connection.DisconnectEvent;
 import com.velocitypowered.api.event.connection.PostLoginEvent;
 import com.velocitypowered.api.event.connection.PreLoginEvent;
+import com.velocitypowered.api.event.player.ServerPreConnectEvent;
 import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.util.GameProfile;
+import com.velocitypowered.api.util.UuidUtils;
 import me.biglev.velocityauth.Main;
+import me.biglev.velocityauth.utils.ComponentFormat;
 import me.biglev.velocityauth.utils.Core;
-import me.biglev.velocityauth.utils.api.PlayerAPI;
+import me.biglev.velocityauth.utils.api.PlayerProfile;
+import me.biglev.velocityauth.utils.settings.Manager;
+
+import java.lang.reflect.Field;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class PlayerLogin {
+
+    @Subscribe(order = PostOrder.FIRST)
+    public void preLogin(PreLoginEvent e) {
+        Core.connectionHandler(e);
+    }
+
+    @Subscribe(order = PostOrder.NORMAL)
+    public void preConnect(ServerPreConnectEvent e) {
+        Player player = e.getPlayer();
+
+        if (Manager.getSettings().getPremiumAuthentication().isOfflineUUID()) {
+            PlayerProfile playerProfile = Main.getPlayerAPIList().searchPlayer(player.getUsername());
+            if (playerProfile.isPremium()) {
+                try {
+                    GameProfile gameProfile = player.getGameProfile();
+                    Field field = gameProfile.getClass().getDeclaredField("id");
+                    field.setAccessible(true);
+                    field.set(gameProfile, UuidUtils.generateOfflinePlayerUuid(player.getUsername()));
+
+                } catch (NoSuchFieldException | IllegalAccessException n) {
+                    n.printStackTrace();
+                }
+            }
+        }
+    }
 
     @Subscribe(order = PostOrder.NORMAL)
     public void onLogin(PostLoginEvent e) {
         Player player = e.getPlayer();
 
-        Core.playerExists(player);
-    }
+        String regex = "^[a-zA-Z0-9]{" + Manager.getSettings().getRestrictions().getMinNicknameLength() + "," + Manager.getSettings().getRestrictions().getMaxNicknameLength() + "}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(player.getUsername());
 
-    @Subscribe(order = PostOrder.NORMAL)
-    public void preLogin(PreLoginEvent e) {
-//        e.setResult(PreLoginEvent.PreLoginComponentResult.forceOnlineMode());
+        if (!matcher.matches()) {
+            player.disconnect(ComponentFormat.format(Manager.getMessage().getError_messages().getName_length()));
+        }
+        Core.playerExists(player);
     }
 
     @Subscribe(order = PostOrder.NORMAL)
     public void isLeave(DisconnectEvent e) {
         Player player = e.getPlayer();
-
-        PlayerAPI playerAPI = Main.getPlayerAPIList().searchPlayer(player.getUsername());
-        playerAPI.setLogged(false);
+        PlayerProfile playerProfile = Main.getPlayerAPIList().searchPlayer(player.getUsername());
+        playerProfile.setLogged(false);
     }
 }
